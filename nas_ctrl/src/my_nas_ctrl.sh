287d396c6aa1c84499523fee5158650a12952d6c
zcchen <chen.bill.bill@gmail.com>
2017-08-21 22:50:37 +0800

    Prepare to make a better nas_control scripts and samba / sftp config files

--- nas_ctrl/src/my_nas_ctrl.sh
+++ nas_ctrl/src/my_nas_ctrl.sh
@@ -1,39 +1,6 @@
 #!/bin/bash
 
-GRP_NAME="family"               # the group name
-PUBLIC_NAME="public"
-
-DATA_PATH='/tmp/data/'
-PUBLIC_DIRs=( \
-    "Movies_电影"
-    "Music_音乐"
-    "Picture_贴图"
-    "Games_游戏"
-    "Software_软件"
-    "eBooks_电子书"
-    "Downloads_下载"
-)   # ${DATA_PATH}/${GRP_NAME}/${PUBLIC_NAME}/PUBLIC_DIRs
-GRP_DIRs=( \
-    "Photos_照片"
-    "Public_公共文件"
-)   # ${DATA_PATH}/${GRP_NAME}/${GRP_NAME}/GRP_DIRs
-USER_SKEL_DIRs=( \
-    "Photos_照片/personal_个人照片"
-)   # ${DATA_PATH}/${GRP_NAME}/.skel/
-USER_SKEL_LNs=( \
-    "Movies_电影:../${PUBLIC_NAME}/Movies_电影"
-    "Music_音乐:../${PUBLIC_NAME}/Music_音乐"
-    "Picture_贴图:../${PUBLIC_NAME}/Picture_贴图"
-    "Games_游戏:../${PUBLIC_NAME}/Games_游戏"
-    "Software_软件:../${PUBLIC_NAME}/Software_软件"
-    "eBooks_电子书:../${PUBLIC_NAME}/eBooks_电子书"
-    "Downloads_下载:../${PUBLIC_NAME}/Downloads_下载"
-    "Public_公共文件:../${GRP_NAME}/Public_公共文件"
-    "Photos_照片/family_家庭照片:../../${GRP_NAME}/Photos_照片"
-)   # ${DATA_PATH}/${GRP_NAME}/.skel/
-
-ANONYMOUS_UPLOAD="upload"       # anonymous upload name
-UPLOAD_DIR='upload'
+#__config_data__#
 
 EMPTY_DIR_GEN=$(mktemp -d)  # change the empty_dir generate way as you like
 

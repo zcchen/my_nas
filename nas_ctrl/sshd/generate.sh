27bf40c36a678611406669b49e732074c94b6312
zcchen <chen.bill.bill@gmail.com>
2017-08-23 10:40:44 +0800

    Added a script to generate the sshd_config for sftp.

---
+++ nas_ctrl/sshd/generate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+    echo "$0 <config.sh>"
+    exit 1
+fi
+realbase=$(realpath $(dirname $0))
+realconfig=$(realpath $1)
+source ${realconfig}
+
+sed_cmd=( \
+    "s,__SSHD_PORT__,${SSHD_PORT},g;" \
+    "s,__DATA_PATH__,${DATA_PATH},g;" \
+    "s,__GRP_NAME__,${GRP_NAME},g;" \
+)
+
+sed -e "${sed_cmd[*]}" ${realbase}/sshd_config

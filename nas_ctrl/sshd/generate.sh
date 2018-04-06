9998dbdf4b7666450959d50ffc79eab610a598e8
zcchen <chen.bill.bill@gmail.com>
2018-04-06 22:09:19 +0800

    Add one line for the sshd port

--- nas_ctrl/sshd/generate.sh
+++ nas_ctrl/sshd/generate.sh
@@ -9,7 +9,8 @@ realconfig=$(realpath $1)
 source ${realconfig}
 
 sed_cmd=( \
-    "s,__SSHD_PORT__,${SSHD_PORT},g;" \
+    "s,__SSHD_PORT1__,${SSHD_PORT},g;" \
+    "s,__SSHD_PORT2__,${SSHD_PORT},g;" \
     "s,__DATA_PATH__,${DATA_PATH},g;" \
     "s,__GRP_NAME__,${GRP_NAME},g;" \
     "s,//,/,g;"
